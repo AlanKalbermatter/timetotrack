0c79f1cb05a06669e825710c92b965006a1498ee
Alan Kalbermatter <68169366+AlanKalbermatter@users.noreply.github.com>
2020-09-04 00:48:40 -0300

    JDBC controllers

---
+++ timetotrack/src/main/java/com/alan/dev/timetotrack/Cap_userController.java
@@ -0,0 +1,27 @@
+package com.dev.timetotrack.timetotrack;
+
+import io.vertx.core.Vertx;
+import io.vertx.ext.web.RoutingContext;
+
+public class Cap_userController {
+
+    public Cap_userController(Vertx vertx) {
+
+    }
+
+	public void addCap_user(RoutingContext routingContext) {
+
+	}
+
+	public void getOne(RoutingContext routingContext) {
+	}
+
+	public void updateOne(RoutingContext routingContext) {
+	}
+
+	public void deleteCap_user(RoutingContext routingContext) {
+	}
+
+	public void getAll(RoutingContext routingContext) {
+	}
+}
