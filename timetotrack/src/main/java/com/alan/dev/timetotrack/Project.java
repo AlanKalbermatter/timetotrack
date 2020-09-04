56d29d8e1d2539f519e9bb59784e2c33597aeed6
Alan Kalbermatter <68169366+AlanKalbermatter@users.noreply.github.com>
2020-09-04 00:40:34 -0300

    Add files via upload

--- timetotrack/src/main/java/com/alan/dev/timetotrack/Project.java
+++ timetotrack/src/main/java/com/alan/dev/timetotrack/Project.java
@@ -1,71 +1,43 @@
-package com.alan.dev.timetotrack;
-
-import io.vertx.core.json.JsonObject;
+package com.dev.timetotrack.timetotrack;
 
 public class Project {
 
-	private int projects_id;
+  private int projects_id;
+  private String projects_name;
+  public int customer_id;
 
-	private String projects_name;
+  public Project(){
+    //default constructor
+  }
 
-	private int customer_id;
+  public Project(final int projects_id, final String projects_name, final int customer_id) {
+    this.projects_id = projects_id;
+    this.projects_name = projects_name;
+    this.customer_id = customer_id;
+  }
 
-	String id = String.valueOf(Integer.parseInt(""));
+    public int getProjects_id() {
+        return projects_id;
+    }
 
-	public Project(String projects_name, int customer_id) {
-		this.projects_name = projects_name;
-		this.customer_id = customer_id;
-		this.projects_id = Integer.parseInt("");
-	}
+    public void setProjects_id(int projects_id) {
+        this.projects_id = projects_id;
+    }
 
-	public Project(JsonObject json) {
-		this.projects_name = json.getString("projects_name");
-		this.customer_id = json.getInteger("projects_id");
-		this.projects_id = json.getInteger("customer_id");
-	}
+    public String getProjects_name() {
+        return projects_name;
+    }
 
-	public Project() {
-		this.projects_id = Integer.parseInt("");
-	}
+    public void setProjects_name(String projects_name) {
+        this.projects_name = projects_name;
+    }
 
-	public Project(int projects_id, String projects_name, int customer_id) {
-		this.projects_id = projects_id;
-		this.projects_name = projects_name;
-		this.customer_id = customer_id;
-	}
+    public int getCustomer_id() {
+        return customer_id;
+    }
 
-	public JsonObject toJson() {
-		JsonObject json = new JsonObject().put("name", projects_name).put("customer id", customer_id);
-		if (id != null && !id.isEmpty()) {
-			json.put("_project_id", projects_id);
-		}
-		return json;
-	}
+    public void setCustomer_id(int customer_id) {
+        this.customer_id = customer_id;
+    }
 
-	public String getProjects_name() {
-		return projects_name;
-	}
-
-	public int getProjects_id() {
-		return projects_id;
-	}
-
-	public int getCustomer_id() {
-		return customer_id;
-	}
-
-	public Project setProject_name(String projects_name) {
-		this.projects_name = projects_name;
-		return this;
-	}
-
-	public Project setProject_id(int projects_id) {
-		this.projects_id = projects_id;
-		return this;
-	}
-
-	public Project setCustomer_id(int customer_id) {
-		this.customer_id = customer_id;
-		return this;
-	}
 }
